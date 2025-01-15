4709868cff282b9714341c6f283eeda1e7cc6e75
mbprograming2024 <mbprograming2024@gmail.com>
2025-01-15 20:43:06 +0530

    enumurate concept

---
+++ enumurate_concept/concept.py
@@ -0,0 +1,15 @@
+fruit = ["apple","banana","mango","pineapple","pomogranate"]
+
+
+
+# for x in fruit:
+#     print(x)
+
+
+# # with enumurate 
+# for index,value in enumerate(fruit):
+#     print(f"index {index}:{value}")
+
+
+# for i in range(len(fruit)):
+#     print("index"+str(i)+":"+fruit[i])
